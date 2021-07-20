5ef359a45cb5a95e2b68bcce328813eba86c6610
Esha-Sherring <esherring2016@gmail.com>
2021-07-20 23:47:58 +0530

    CSS changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Text from './components/Controls/Text';
 import Output from './components/Output'
 import Select from './components/Controls/Select';
+import './App.css';
 //import { set } from 'harmony-reflect';
 
 export default class App extends Component {
@@ -10,7 +11,7 @@ export default class App extends Component {
      super(props)
      this.state ={
        text :" ",
-       paras: 4,
+       paras: 6,
        html : 'html'
      }
   }
@@ -42,23 +43,30 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div className="App container">
+      <div  className="App" >
+      <div  style={{margin:"1rem",padding:"2rem"}} className="App container">
         <h1 className="text-center">React text generator</h1>
-         <form className="form-inline">
+        <div style={{margin:"1 rem",marginBottom:"1rem"}}>
+           <form  className="form-inline">
            <div className="form-group">
-            <label>Paragraphs:</label>
+          <label>Paragraphs:</label>
            <Text value={this.state.paras} onChange={this.changeParas.bind(this)}/>
-          
-           
            </div>
             <div className="form-group">
            <label>Format:</label>
            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
             </div>
          </form>
-         <Output value={this.state.text} />
+        </div>
+         
+         <div  style={{backgroundColor:"white", textAlign:"justify",padding:"1rem",margin:"1 rem"}} className="output">
+            <Output  value={this.state.text} /> 
+         </div>
+         
        
       </div>
+      </div>
+      
     )
   }
 }
